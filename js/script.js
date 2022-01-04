72345318889c29f2dd4d90d1d267507cf9e638b8
aximas <northdakota778@gmail.com>
2022-01-05 00:35:29 +0500

    FIX: mobile menu fixed;

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
 
-    // const menuButton = document.querySelector('.mobile-menu-toggle'),
-    //     menuButtonClose = document.querySelector('.mobile-menu-close'),
-    //     mobileView = document.querySelectorAll('.mobile-view'),
-    //     headerNav = document.querySelector('.header-nav');
-    //
-    // menuButton.addEventListener('click', () => {
-    //     headerNav.classList.add('menu-display-toggle');
-    // });
-    //
-    // menuButtonClose.addEventListener('click', () => {
-    //     headerNav.classList.remove('menu-display-toggle');
-    // });
-    //
-    // if (window.innerWidth > 1024) {
-    //     mobileView.forEach(element => {
-    //         element.style.display = 'none';
-    //     });
-    // }
+    const menuButtonToggle = document.querySelector('.mobile-menu-toggle'),
+        menu = document.querySelector('.header__menu'),
+        body = document.querySelector('body');
+
+    menuButtonToggle.addEventListener('click', (event) => {
+        if (menu.classList.contains('hide-mobile-menu')) {
+            menu.classList.remove('hide-mobile-menu');
+            event.currentTarget.classList.add('mobile-menu-toggle-close');
+            body.style.overflowY = 'hidden';
+        } else {
+            menu.classList.add('hide-mobile-menu');
+            event.currentTarget.classList.remove('mobile-menu-toggle-close');
+            body.style.overflowY = '';
+
+        }
+    })
 
 })
